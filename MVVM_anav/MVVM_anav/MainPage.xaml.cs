0b274b33d7c1fea434e9608b804623691c7c9afc
Angelica <Angelica@DESKTOP-TBNRI07>
2023-10-16 08:04:19 -0700

    Add project files.

---
+++ MVVM_anav/MVVM_anav/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MVVM_anav
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+            Alerta();
+        }
+        private void Alerta()
+        {
+            DisplayAlert("Alerta", "Esto es un alerta", "Okay");
+        }
+    }
+}
